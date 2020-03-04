c3da3f49d479d5492390339c1e0fdd7c749f5c0d
Sal <saljan93@live.com>
2020-03-04 14:07:44 +0100

    Google stock price | date annoyances

--- tutorial/basic/ex7.py
+++ tutorial/basic/ex7.py
@@ -4,12 +4,15 @@ import pandas as pd
 
 from sal_timer import timer
 
-
+import matplotlib.cbook as cbook
+import matplotlib.dates as mdates
 
 def plot_1(df):
-    plt.subplot(111)
-    plt.plot('Open', data=df)
-    # plt.plot('Date','Open', data=df)
+    fig, ax = plt.subplots()
+    # plt.plot('Open', data=df)
+    plt.plot('Date','Open', data=df)
+    fig.autofmt_xdate()
+
     plt.title('Google')
     plt.show()
 
@@ -21,6 +24,7 @@ def plot_1(df):
 @timer
 def main():
     df = pd.read_csv(DATA_PATH)
+    df['Date'] = pd.to_datetime(df['Date'])
     print(df.shape)
     print(df.head())
     plot_1(df)
